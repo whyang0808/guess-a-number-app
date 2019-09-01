2354baf5f0a164f6f7d22e6c154d7ea53490fc4d
Yang Wei Hao <whyang0808@gmail.com>
2019-09-01 18:26:14 +0800

    Style a View as a 'Card' Container (with Drop Shadows and Rounded Corners)

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,13 +4,13 @@ import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text style={styles.title}>The Game Screen</Text>
+            <Text style={styles.title}>Start a New Game!</Text>
             <View style={styles.inputContainer}>
                 <Text>Select a Number</Text>
-                <TextInput/>
+                <TextInput />
                 <View style={styles.buttonContainer}>
-                    <Button title="Reset" onPress={() => {}}/>
-                    <Button title="Confirm" onPress={() => {}}/>
+                    <Button title="Reset" onPress={() => { }} />
+                    <Button title="Confirm" onPress={() => { }} />
                 </View>
             </View>
         </View>
@@ -24,12 +24,24 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     title: {
-        fontSize: 20
+        fontSize: 20,
+        marginVertical: 10
     },
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center'
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        elevation: 8,
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10
     },
     buttonContainer: {
         flexDirection: 'row',
